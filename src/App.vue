9e0576a6773a440e347b5260b0fdf99803cc3faa
shjiangc <2016321959@qq.com>
2024-08-05 23:20:41 +0800

    333

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="all" @click="num++">
+        <h1>吴梦如是sabi</h1>
+        <h2>{{ num }}</h2>
+        <img src="../src/assets/vue.svg" alt="">
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+import './JC/index.jc'
+let num=ref(1)
+</script>
+
+<style scoped lang="scss">
+.all{
+    background: pink;
+    h2{
+        color: aqua;
+    }
+}
+</style>
